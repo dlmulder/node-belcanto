30247b36b220e42a0abb2965264b33e06fc03d58
Dave Mulder <dlmulder@gmail.com>
2026-03-12 17:58:20 -0500

    WIP

--- lib.js
+++ lib.js
@@ -6,33 +6,48 @@ let util = require("util"),
 events = require('events');
 const {usb} = require('usb');
 
-function McIntosh() {
+function BelCanto() {
     this.seq = 0;
+    this.volume = -1
+    console.log("[BelCanto lib.js] creating BelCanto instance");
 }
 
-util.inherits(McIntosh, events.EventEmitter);
+util.inherits(BelCanto, events.EventEmitter);
 
 let _processw = function () {
     if (!this._port)
-        return;
+        return
     if (this._woutstanding)
-        return;
+        return
     if (this._qw.length == 0)
-        return;
+        return
 
     this._woutstanding = true;
-    console.log("[McIntosh] writing:", this._qw[0]);
+    if(this._qw[0].length <= 4)
+        console.log("[BelCanto] writing", this._qw[0].length, ":",
+            this._qw[0][0].toString(16),
+            this._qw[0][1].toString(16),
+            this._qw[0][2].toString(16),
+            this._qw[0][3].toString(16))
+    else
+        console.log("[BelCanto] writing", this._qw[0].length, ":",
+            this._qw[0][0].toString(16),
+            this._qw[0][1].toString(16),
+            this._qw[0][2].toString(16),
+            this._qw[0][3].toString(16),
+            this._qw[0][4].toString(16))
 
-    this._port.write(this._qw[0] + "\n",
+    this._port.write(
+        this._qw[0],
         (err) => {
-        if (err)
-            return;
-        this._qw.shift();
-        this._woutstanding = false;
-        setTimeout(() => {
-            _processw.call(this);
-        }, 150);
-    });
+            if (err)
+                return;
+            this._qw.shift();
+            this._woutstanding = false;
+            setTimeout(() => {
+                _processw.call(this);
+            }, 150);
+        });
 }
 
 function send(val, cb) {
@@ -40,79 +55,222 @@ function send(val, cb) {
     _processw.call(this);
 };
 
-McIntosh.prototype.volume_up = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(VUP Z1)\n");
-    } else {
-        send.call(this, "(VOL U)\n");
-    }
-};
-McIntosh.prototype.volume_down = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(VDN Z1)\n");
-    } else {
-        send.call(this, "(VOL D)\n");
+// codes from
+// https://www.plaudio.com/pdf/docs/Belcanto%20AMiP%20RS232%20Codes%20v1.2.pdf
+// https://www.manualslib.com/manual/15208/Bel-Canto-Pl-1.html?page=71#manual
+
+// packet construction
+const DLE = 0x10
+const XORCHR = 0x40
+
+// first byte Flag
+const FLAG = 0x7e
+
+// second byte Type
+const TYPE_MSB = 0x80
+const TYPE_STATUS = 0x20    // 1=Ack/Nack, 0=other
+const  TYPE_ACK = 0x06
+const  TYPE_NACK = 0x15
+const TYPE_SIZE_MASK = 0x0f // number of data bytes - 1
+
+// third byte Command
+const COMMAND_MSB = 0x80
+const COMMAND_READ = 0x40   // 1=Read, 0=Write
+const COMMAND_MASK = 0x3f
+const COMMAND = {
+    Display:2, Mute:3, Input:5, Volume:7, Balance:9, AckRxWrite:100, NackRxWrite:101
+}
+
+// subsequent bytes are Data
+
+// final byte is Checksum (Type + Command + Data[]) Mod 256 (<before> DLE suppression)
+
+const MUTE = {
+    OFF:0xe0, ON:0xe1, SOFT:0xe2
+}
+
+function make_tx_command(command, data, read_not_write=false) {
+    if(command == COMMAND.AckRxWrite)
+        return make_tx_command_(rx_packet.command, TYPE_STATUS, [TYPE_ACK], false)
+    else if(command == COMMAND.NackRxWrite)
+        return make_tx_command_(rx_packet.command, TYPE_STATUS, [TYPE_NACK], false)
+    else
+        return make_tx_command_(command, 0, data, read_not_write)
+}
+function make_tx_command_(command, type, data, read_not_write) {
+    if(typeof(data) == "number")
+        data = [data]
+    let datacount = data.length
+    if(datacount > 0)
+        datacount -= 1
+
+    let cmd = [FLAG]
+
+    let type_byte = TYPE_MSB | type | datacount
+    cmd.push(type_byte)
+
+    let command_byte = COMMAND_MSB | (read_not_write?COMMAND_READ:0) | command
+    cmd.push(command_byte)
+    for(let i = 0; i <= datacount; ++i) {
+        let b = data[i]
+        if((b == DLE) || (b == FLAG)) {
+            cmd.push(DLE)
+            b ^= XORCHR
+        }
+        cmd.push(b)
     }
 
+    let checksum = type_byte
+    checksum += command_byte
+    for(let i = 0; i <= datacount; ++i) {
+        checksum += data[i]
+    }
+    cmd.push(checksum % 256)
+
+    return cmd
+}
+
+// the most recent received data, separated into fields
+let rx_packet = {
+    type:0, command:0, data:0, checksum:0
+}
+const RX_PACKET_STATE = { rx_idle:0, rx_type:1, rx_command:2, rx_data:3, rx_data_dle:4, rx_checksum:5, rx_checksum_dle:6 }
+let rx_packet_state = RX_PACKET_STATE.rx_idle
+let rx_packet_checksum = 0
+let rx_packet_datalen = 0
+function parse_rx_byte(b) {
+    switch(rx_packet_state) {
+    case RX_PACKET_STATE.rx_idle:
+        if(b == FLAG)
+            rx_packet_state = RX_PACKET_STATE.rx_type
+        break;
+    case RX_PACKET_STATE.rx_type:
+        if((b & TYPE_MSB) == TYPE_MSB) {
+            rx_packet_checksum = b
+            rx_packet.type = b
+            rx_packet_datalen = b & TYPE_SIZE_MASK
+            if((rx_packet_datalen > 0) && (rx_packet_datalen <= 16))
+                rx_packet_state = RX_PACKET_STATE.rx_command
+            else
+                rx_packet_state = RX_PACKET_STATE.rx_idle
+        } else {
+            rx_packet_state = RX_PACKET_STATE.rx_idle
+        }
+        break;
+    case RX_PACKET_STATE.rx_command:
+        if((b & COMMAND_MSB_MSB) == COMMAND_MSB) {
+            rx_packet.command = b
+            rx_packet_checksum += b
+
+            rx_packet_state = RX_PACKET_STATE.rx_data
+        } else
+            rx_packet_state = RX_PACKET_STATE.rx_idle
+        break;
+    case RX_PACKET_STATE.rx_data:
+        if(b != DLE) {
+            if(rx_packet_datalen > 0) {
+                --rx_packet_datalen
+                rx_packet.data[rx_packet_datalen] = b
+                rx_packet_checksum += b
+
+                if(rx_packet_datalen == 0)
+                    rx_packet_state = RX_PACKET_STATE.rx_checksum
+            } else
+                rx_packet_state = RX_PACKET_STATE.rx_idle
+        }
+        else
+            rx_packet_state = RX_PACKET_STATE.rx_data_dle   // throw away this DLE byte
+        break;
+    case RX_PACKET_STATE.rx_data_dle:
+        --rx_packet_datalen
+        rx_packet.data[rx_packet_datalen] = b ^ XORCHR
+        rx_packet_checksum += b ^ XORCHR
+
+        if(rx_packet_datalen == 0)
+            rx_packet_state = RX_PACKET_STATE.rx_checksum
+        else
+            rx_packet_state = RX_PACKET_STATE.rx_data
+        break;
+    case RX_PACKET_STATE.rx_checksum:
+        if(b != DLE) {
+            if(b == rx_packet.checksum % 256)
+                handle_rx_packet()
+            else
+                rx_packet_state = RX_PACKET_STATE.rx_idle
+        } else
+            rx_packet_state = RX_PACKET_STATE.rx_checksum_dle
+        break;
+    case RX_PACKET_STATE.rx_checksum_dle:
+        if(b == rx_packet.checksum % 256)
+            handle_rx_packet()
+        else
+            rx_packet_state = RX_PACKET_STATE.rx_idle
+        break;
+    }
+}
+function handle_rx_packet() {
+    if((rx_packet.type & TYPE_ACK) == TYPE_NACK) {
+    }
+    else {  // ACKs or pushed values
+        switch(rx_packet.command) {
+        case COMMAND.Display:
+            break;
+        case COMMAND.Mute:
+            break;
+        case COMMAND.Input:
+            break;
+        case COMMAND.Volume:
+            break;
+        case COMMAND.Balance:
+            break;
+    }
+    }
+}
+
+BelCanto.prototype.volume_up = function () {
+    if(this.volume < 0)
+        send.call(this, make_tx_command(COMMAND.Volume, 0, true))
+    else
+        send.call(this, make_tx_command(COMMAND.Volume, this.volume + 1));
 };
-McIntosh.prototype.set_volume = function (val) {
+BelCanto.prototype.volume_down = function () {
+    if(this.volume < 0)
+        send.call(this, make_tx_command(COMMAND.Volume, 0, true))
+    else
+        send.call(this, make_tx_command(COMMAND.Volume, this.volume - 1));
+};
+BelCanto.prototype.set_volume = function (val) {
+    console.log("[BelCanto lib.js] executing prototype.set_volume");
     if (this.properties.volume == val)
         return;
     if (this.volumetimer)
         clearTimeout(this.volumetimer);
     var self = this;
     this.volumetimer = setTimeout(() => {
-        if (self.serialCommandMode == "Zone") {
-            send.call(this, "(VST Z1 " + val + ")\n");
-        } else {
-            send.call(this, "(VOL " + val + ")\n");
-        }
+        send.call(this, make_tx_command(COMMAND.Volume, val));
     }, 50)
 };
-McIntosh.prototype.get_status = function () {
-    send.call(this, "(QRY)\n");
+BelCanto.prototype.get_status = function () {
+    // unimplemented
 };
-McIntosh.prototype.power_off = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(POF Z1)\n");
-    } else {
-        send.call(this, "(PWR 0)\n");
-    }
-    let val = "Standby";
-    if (this.properties.source != val) {
-        this.properties.source = val;
-        this.emit('source', val);
-    }
-
+BelCanto.prototype.power_off = function () {
+    // unimplemented
 };
-McIntosh.prototype.power_on = function () {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(PON Z1)\n");
-    } else {
-        send.call(this, "(PWR 1)\n");
-    }
-
+BelCanto.prototype.power_on = function () {
+    // unimplemented
 };
-McIntosh.prototype.set_source = function (val) {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(INP Z1 " + val + ")\n");
-    } else {
-        send.call(this, "(INP " + val + ")\n");
-    }
+BelCanto.prototype.set_source = function (val) {
+    send.call(this, make_tx_command(COMMAND.Input, val));
 };
-McIntosh.prototype.mute = function (val) {
-    if (this.serialCommandMode == "Zone") {
-        send.call(this, "(MUT Z1 " + val + ")\n");
-    } else {
-        send.call(this, "(MUT " + val + ")\n");
-    }
+BelCanto.prototype.mute = function (val) {
+    send.call(this, make_tx_command(COMMAND.Mute, val));
 };
 
-McIntosh.prototype.raw_command = function (val) {
-        send.call(this, val + "\n");
+BelCanto.prototype.raw_command = function (val) {
+    // unimplemented
 };
 
-McIntosh.prototype.init = function (opts, closecb) {
+BelCanto.prototype.init = function (opts, closecb) {
     let self = this;
 
     this._qw = [];
@@ -136,14 +294,14 @@ McIntosh.prototype.init = function (opts, closecb) {
     });
     this._port.pipe(this._parser);
 
+    console.log("[BelCanto] subscribing to 'data'")
     this._parser.on('data', data => {
         if (this.initializing) {
             this.initializing = false;
             this.emit('connected');
         }
-        data = data.trim();
-        console.log('[McIntosh] received: %s', data);
-
+        console.log('[BelCanto] received: %s', data.toHex());
+/*
         if (/^\(VST.* ([0-9]*)$/.test(data)) {
             let val = Number(data.trim().replace(/^\(VST.* ([0-9]*)$/, "$1"));
             if (this.properties.volume != val) {
@@ -207,101 +365,95 @@ McIntosh.prototype.init = function (opts, closecb) {
                 console.log('No matching string');
             }
         }
+*/
     });
-        let timer = setTimeout(() => {
-            if (this.initializing) {
-                this.initializing = false;
-                this.emit('connected');
-            }
-        }, 3000);
-        this._port.on('open', err => {
-            this.emit('preconnected');
+
+    let timer = setTimeout(() => {
+        if (this.initializing) {
+            this.initializing = false;
+            this.emit('connected');
+        }
+    }, 3000);
+    this._port.on('open', err => {
+        this.emit('preconnected');
+        let val = "Standby";
+        this.properties.source = val;
+        //get volume in case device is running (QRY does not report volume, so we need to use a 'trick')
+        send.call(this, make_tx_command(COMMAND.Volume, 0, true))
+    });
+
+    //detection of BelCanto USB disconnection (at power-off)
+    usb.on('detach', device => {
+        if (this.properties.usbVid == device.deviceDescriptor.idVendor) {
+            console.log('remove', device);
             let val = "Standby";
-            this.properties.source = val;
-            //get device status in case it's up
-            send.call(this, "(QRY)\n");
-            //get volume in case device is running (QRY does not report volume, so we need to use a 'trick')
-            send.call(this, "(VDN Z1)\n");
-            send.call(this, "(VUP Z1)\n");
-            send.call(this, "(VOL D)\n");
-            send.call(this, "(VOL U)\n");
-            //        send.call(this, "(PON Z1)\n");
-            //        send.call(this, "(INP Z1 " + this.properties.source + ")\n");
-            //        send.call(this, "(VST Z1 " + this.properties.volume + ")\n");
-        });
-
-        //detection of McIntosh USB disconnection (at power-off)
-        usb.on('detach', device => {
-            if (this.properties.usbVid == device.deviceDescriptor.idVendor) {
-                console.log('remove', device);
-                let val = "Standby";
-                if (this.properties.source != val) {
-                    this.properties.source = val;
-                    this.emit('source', val);
-                }
+            if (this.properties.source != val) {
+                this.properties.source = val;
+                this.emit('source', val);
             }
-        });
+        }
+    });
 
-        this._port.on('close', () => {
-            this._port.close(() => {
-                this._port = undefined;
-                if (closecb) {
-                    var cb2 = closecb;
-                    closecb = undefined;
-                    cb2('close');
-                }
-            })
-        });
-        this._port.on('error', err => {
-            this._port.close(() => {
-                this._port = undefined;
-                if (closecb) {
-                    var cb2 = closecb;
-                    closecb = undefined;
-                    cb2('error');
-                }
-            })
-        });
-        this._port.on('disconnect', () => {
-            this._port.close(() => {
-                this._port = undefined;
-                if (closecb) {
-                    var cb2 = closecb;
-                    closecb = undefined;
-                    cb2('disconnect');
-                }
-            })
-        });
-    };
-
-        McIntosh.prototype.start = function (opts) {
-        this.seq++;
-
-        let closecb = (why) => {
-            this.emit('disconnected');
-            if (why != 'close') {
-                var seq = ++this.seq;
-                setTimeout(() => {
-                    if (seq != this.seq)
-                        return;
-                    this.start(opts);
-                }, 1000);
+    this._port.on('close', () => {
+        this._port.close(() => {
+            this._port = undefined;
+            if (closecb) {
+                var cb2 = closecb;
+                closecb = undefined;
+                cb2('close');
             }
-        };
+        })
+    });
+    this._port.on('error', err => {
+        this._port.close(() => {
+            this._port = undefined;
+            if (closecb) {
+                var cb2 = closecb;
+                closecb = undefined;
+                cb2('error');
+            }
+        })
+    });
+    this._port.on('disconnect', () => {
+        this._port.close(() => {
+            this._port = undefined;
+            if (closecb) {
+                var cb2 = closecb;
+                closecb = undefined;
+                cb2('disconnect');
+            }
+        })
+    });
+};
 
-        if (this._port) {
-            this._port.close(() => {
-                this.init(opts, closecb);
-            });
-        } else {
-            this.init(opts, closecb);
+BelCanto.prototype.start = function (opts) {
+    this.seq++;
+
+    let closecb = (why) => {
+        this.emit('disconnected');
+        if (why != 'close') {
+            var seq = ++this.seq;
+            setTimeout(() => {
+                if (seq != this.seq)
+                    return;
+                this.start(opts);
+            }, 1000);
         }
     };
 
-        McIntosh.prototype.stop = function () {
-        this.seq++;
-        if (this._port)
-            this._port.close(() => {});
-    };
+    if (this._port) {
+        this._port.close(() => {
+            this.init(opts, closecb);
+        });
+    } else {
+        this.init(opts, closecb);
+    }
+};
 
-        exports = module.exports = McIntosh;
+BelCanto.prototype.stop = function () {
+    this.seq++;
+    if (this._port)
+        this._port.close(() => {});
+};
+
+exports = module.exports = BelCanto;
